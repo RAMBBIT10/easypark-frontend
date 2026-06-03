30a6cb716b086f6fe32902f901e6c385e79ed772
RAMBBIT10 <alejito_vv2001@hotmail.com>
2026-06-03 17:10:35 -0500

    feat: integrar Auth0 como Identity Provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
@@ -10,6 +11,13 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideAuth0({
+      domain: 'dev-fzdzwkcpgueyww5e.us.auth0.com',
+      clientId: 'trl6ugAyGyvS4vvmyUtdvziqXpLra0G6',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
   ]
 };
